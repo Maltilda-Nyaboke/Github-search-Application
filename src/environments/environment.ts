2ec85f66d852e18d4820214243abb33c9e4ac248
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-17 01:16:09 +0300

    feature:add access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessToken:'ghp_16C7e42F292c6912E7710c838347Ae178B4a'
 };
 
 /*
