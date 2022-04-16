6085008360dfd4cd338f916ecb51ae40c603bbbc
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-16 23:13:14 +0300

    style:add http

--- src/app/api-service.ts
+++ src/app/api-service.ts
@@ -1,12 +1,18 @@
 
 import {HttpClient} from '@angular/common/http'
+import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util'
+import { Injectable } from '@angular/core'
 
 
 
 
-
+Injectable({
+    providedIn: 'root'
+})
 export class ApiService {
+    github_url:string ='https://api.github.com/'
 constructor(private http:HttpClient){
-
+ 
+    
 }
 }
