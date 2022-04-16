bb0b41f106cae5b6ea976b9602f2589ca124a92a
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-17 01:03:45 +0300

    style:add user and repo elements

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,4 +1,6 @@
 export interface Repository {
 id:number;
 name:string;
+languages: string;
+postDate: Date;
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,8 @@
  export interface User {
+avatar: string
 name:string;
 username:string;
 followers:number;
 following:number;
+repositories:number;
 }
