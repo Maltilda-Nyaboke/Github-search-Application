26d702431afd7aafcf0369b66bbf45f1f41fe117
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-17 00:50:27 +0300

    style:add user interface

--- src/app/api-service.ts
+++ src/app/api-service.ts
@@ -2,7 +2,8 @@
 import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {User} from './user'
+import {User} from './user';
+
 
 
 
@@ -13,12 +14,12 @@ Injectable({
 export class ApiService {
     github_url:string ='https://api.github.com/'
 constructor(private http:HttpClient){
- 
+}
   getUsers():Observable<User>{
-
+    return this.http.get<User>(this.github_url + 'users')
   } 
-  getRepos() {
+
+  getRepos(){
 
   }
-}
 }
--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,4 @@
-export class Repository {
+export interface Repository {
+id:number;
+name:string;
 }
