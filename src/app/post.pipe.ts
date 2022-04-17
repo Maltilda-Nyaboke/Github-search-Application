7b1e9e20993cc4334052abafb4e9beed38ab1cb5
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-18 02:54:36 +0300

    style:add intervals

--- src/app/post.pipe.ts
+++ src/app/post.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PostPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: unknown, ...args: unknown[]): any {
+    if (value){
+      const intervals:any = {
+        'year': 31536000,
+        'month': 2592000,
+        'week': 604800,
+        'day': 86400,
+        'hour': 3600,
+        'minute': 60,
+        'second': 1
+    };
+    }
   }
 
 }
