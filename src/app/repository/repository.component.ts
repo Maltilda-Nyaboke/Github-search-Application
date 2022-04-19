dbdcdde489e6dc1f0ba12ed5278905b1f1293863
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-19 17:21:16 +0300

    style:card positioning

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Repository } from '../repository';
 import { ApiService } from '../api.service';
 import { UsersComponent } from '../users/users.component';
+import { User } from '../user';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,10 +12,31 @@ import { UsersComponent } from '../users/users.component';
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+  username!:any;
   userRepos!: any;
-  constructor(private UserService: ApiService) { }
-
+  githubUser!: User;
+  constructor(private UserService: ApiService,private router: Router) { }
+  enter() {
+    this.UserService.updateUser(this.username);
+    this.UserService.getUsers().subscribe(
+      data => {
+        this.githubUser = data;
+      }
+    )
+    this.UserService.getRepos().subscribe(
+      repo => {
+        this.userRepos = repo
+      }
+    )
+    this.router.navigateByUrl('repos')
+  }
   ngOnInit(): void {
+    this.UserService.getUsers().subscribe(
+      data => {
+        this.githubUser = data;
+        console.log(this.githubUser);
+      }
+    )
     this.UserService.getRepos().subscribe(
       data => {
         this.userRepos = data;
